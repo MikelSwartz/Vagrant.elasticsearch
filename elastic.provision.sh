3ea3816b604279d38ff641e4369cba0769bb0c6f
Mike <mikelswartz@gmail.com>
2017-01-26 14:26:32 -0500

    Added imdepotency for .rpm files

--- elastic.provision.sh
+++ elastic.provision.sh
@@ -2,11 +2,22 @@
 cd /vagrant/
 yum update
 yum install -y vim git tree wget curl
+if [ -f "/vagrant/jre-8u60-linux-x64.rpm" ]
+then
+        echo "jre-8u60-linux-x64.rpm exists"
+else
+	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+	  "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-linux-x64.rpm"
+fi
 
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-	"http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-linux-x64.rpm"
+if [ -f "/vagrant/elasticsearch-2.4.1.rpm" ]
+then
+        echo "elasticsearch-2.4.1.rpm exists"
+else
+	wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.4.1/elasticsearch-2.4.1.rpm
+fi
+
 yum localinstall jre-8u60-linux-x64.rpm -y
-wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.4.1/elasticsearch-2.4.1.rpm
 yum install elasticsearch-2.4.1.rpm -y
 chkconfig --add elasticsearch
 service elasticsearch start
